9b59754313c28e630c45c14b4bcfa2d7f800feb4
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-11-06 14:30:36 +0100

    Tabellen mit Primary Keys & Foreign Keys

--- api/setup.php
+++ api/setup.php
@@ -1,10 +1,19 @@
 <?php
 
 $db = new SQLite3('./orders.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
-$menu = [];
-$stmt = $db->prepare('create table menuItem(id int auto_increment, name varchar(100), description varchar(255), price double)'); // TODO: SQL query
-$result = $stmt->execute();
 
-/* while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-array_push($menu, $row);
-} */
+$stmt1 = $db->prepare('create table menuItem(id int auto_increment primary key, name varchar(100), description varchar(255), price double)'); // TODO: SQL query
+$result = $stmt1->execute();
+
+$stmt2 = $db->prepare('create table orders(id int auto_increment primary key, created timestamp current_timestamp)');
+$result = $stmt2->execute();
+
+$stmt3 = $db->prepare('create table orderItems(id int auto_increment primary key, order_id int, menu_item_id int, quantity int, foreign key (order_id) references orders(id), foreign key (menu_item_id) references menuItem(id))');
+$result = $stmt3->execute();
+
+
+// OrderItems
+// id
+// order_id
+// menu_item_id
+// quantity
