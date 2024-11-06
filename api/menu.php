fb8ea14e86ee03076a23e588cc3ba2a55822a0b9
aaron <aaronwey@posteo.de>
2024-11-06 15:38:24 +0100

    Adds query

--- api/menu.php
+++ api/menu.php
@@ -1,19 +1,13 @@
 <?php
- $json = [
-["id"=> 1, "name"=> "Vanille", "description"=> "Vanille-Eis", "price"=>1.5 ],
-["id"=> 2, "name"=> "Schokolade", "description"=> "Schokoladen-Eis", "price"=> 1.5 ],
-["id"=> 3, "name"=> "Erdbeer", "description"=> "Erdbeer-Eis", "price"=> 1.5 ]
-]; 
+$db = new SQLite3('./orders.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
-/*
- * Alternativer Code 
- $myObj = new stdClass();
+$menu = [];
+$stmt = $db->prepare('Select * from menuItem');
+$result = $stmt->execute();
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    array_push($menu, $row);
+}
 
-$myObj->name = "John";
-$myObj->age = 30;
-$myObj->city = "New York";
+echo json_encode($menu);
 
-$myJSON = json_encode($myObj); */
-
-echo json_encode($json);
 ?>
