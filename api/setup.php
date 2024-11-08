a5ab829340754f6ab6d0f2f72c0596ef878c13f4
aaron <aaronwey@posteo.de>
2024-11-08 10:30:57 +0100

    Räumt php etwas auf

--- api/setup.php
+++ api/setup.php
@@ -1,25 +1,14 @@
 <?php
 
+
+function execute_sql($sql_as_str) {
+
 $db = new SQLite3('./orders.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+$prepared_statement = $db->prepare($sql_as_str);
+$result = $prepared_statement->execute();
 
-$stmt1 = $db->prepare('create table menuItem(id integer primary key, name varchar(100), description varchar(255), price decimal)'); // TODO: SQL query
-$result = $stmt1->execute();
-
-$stmt2 = $db->prepare('create table orders(id integer primary key, created datetime)');
-$result = $stmt2->execute();
-
-$stmt3 = $db->prepare('create table orderItems(id integer primary key, order_id int, menu_item_id int, quantity int, foreign key (order_id) references orders(id), foreign key (menu_item_id) references menuItem(id))');
-$result = $stmt3->execute();
-
-
-$stmt4 = $db->prepare('insert into menuItem (name, description, price) values
-    ("Vanille", "Milcheis mit echter Bourbon Vanille", 2.99),
-    ("Schokolade", "hoher Anteil an belgischer Schokolade", 2.55),
-    ("Erdbeer", "mit echten Erdbeeren", 2.29)         ');
-$result = $stmt4->execute();    
-
-// OrderItems
-// id
-// order_id
-// menu_item_id
-// quantity
+}
+execute_sql('create table menuItem(id integer primary key, name varchar(100), description varchar(255), price decimal)');
+execute_sql('create table orders(id integer primary key, created datetime)');
+execute_sql('create table orderItems(id integer primary key, order_id int, menu_item_id int, quantity int, foreign key (order_id) references orders(id), foreign key (menu_item_id) references menuItem(id))');
+execute_sql('insert into menuItem (name, description, price) values ("Vanille", "Milcheis mit echter Bourbon Vanille", 2.99), ("Schokolade", "hoher Anteil an belgischer Schokolade", 2.55), ("Erdbeer", "mit echten Erdbeeren", 2.29)');
