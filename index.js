a1d76c177620a0e41b3af9dd2932ea0661d20f41
Ines <link_ines@gmx.de>
2024-11-15 18:04:44 +0100

    Daten werden aus Backend im Frontend angezeigt, html wurde angepasst

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
 document.addEventListener("DOMContentLoaded", async (event) => {
-  console.log("Request succesfully send");
+    console.log("Request succesfully send");
 
-  const url = " http://localhost:8000/api/menu.php";
-  let menuItemsAsJson;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    const url = " http://localhost:8000/api/menu.php";
+    let menuItemsAsJson;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        menuItemsAsJson = await response.json();
+
+        console.log("Request processed");
+    } catch (error) {
+        console.error(error.message);
     }
 
-    menuItemsAsJson = await response.json();
+    console.log(menuItemsAsJson);
 
-    console.log("Request processed");
-  } catch (error) {
-    console.error(error.message);
-  }
+    let container = document.getElementById("innerDiv");
+    let button = document.getElementById("btn");
+    let input = document.getElementById("inputId");
 
-  console.log(menuItemsAsJson);
+    button.addEventListener("click", async () => {
+        console.log("Button clicked.");
+        let btn = document.getElementById("btn");
+        let userChoice = input.value;
+        console.log(userChoice);
+        /* Bei "Klick" erscheint eine Nachricht, dass die Bestellung angekommen ist */
+        document.getElementById("thxOrder").innerHTML = "Danke für Ihre Bestellung";
+        const request = await fetch("http://localhost:8000/api/orders.php", {
+            method: "POST",
+            body: JSON.stringify({ id: userChoice }),
+        });
 
-  let container = document.getElementById("innerDiv");
-  let button = document.getElementById("btn");
-  let input = document.getElementById("inputId");
-
-  button.addEventListener("click", async () => {
-    console.log("Button clicked.");
-    let btn = document.getElementById("btn");
-    let userChoice = input.value;
-    console.log(userChoice);
-    /* Bei "Klick" erscheint eine Nachricht, dass die Bestellung angekommen ist */
-    document.getElementById("thxOrder").innerHTML = "Danke für Ihre Bestellung"
-    const request = await fetch("http://localhost:8000/api/orders.php", {
-      method: "POST",
-      body: JSON.stringify({ id: userChoice }),
+        const result = await request.text();
+        console.log(result);
     });
+    
+    console.log(container);
+    /* erster Versuch  */
+    //container.innerHTML = menuItemsAsJson[1].name + " " + menuItemsAsJson[1].price + "</br>";
 
-    const result = await request.text();
-    console.log(result);
-  });
-  console.log(container);
-  container.innerHTML = menuItemsAsJson[1].name;
+
+    for (let i of menuItemsAsJson) {
+        iceCreamItem = i;
+            container.innerHTML = container.innerHTML + iceCreamItem.name + iceCreamItem.price + "</br>";
+    }
+    // document.getElementById("innerDiv").innerHTML = iceCreamItem
+
+
+
+
+
+
+
+
 });
