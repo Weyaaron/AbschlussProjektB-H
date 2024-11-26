a0f2e211eaf0e5c12dbc217eed7b02ba90dcc676
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-11-26 18:49:05 +0100

    orders.php trägt menu_item und quantity aus JSON ein

--- api/orders.php
+++ api/orders.php
@@ -1,6 +1,10 @@
 <?php
 
+
 $db = new SQLite3('./../orders.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+
+
+
 
 function get_post_data() {
     return json_decode(file_get_contents('php://input'));
@@ -8,26 +12,26 @@ function get_post_data() {
 
 $data = get_post_data();
 
+
+
+
+
 $stmt = $db->prepare('INSERT INTO orders(created) VALUES (CURRENT_TIMESTAMP)');
 $result = $stmt->execute();
 
 $order_id = $db->lastInsertRowID();
-$menu_item_id = $data->id;
-$quantity = $data->quantity;
+// Zugriff auf einzelne Objekte des Arrays oder_data
+foreach ($data->order_data as $entry) { // $entry sind die einzelnen Objekte ...
+    $menu_item_id = $entry->menu_item_id; // ... aus denen ich menu_item_id ...
+    $quantity = $entry->quantity; // ... und quantity abfrage
 
-for ($menu_item_id = 1; $menu_item_id < 4; $menu_item_id++) {
     $stmt2 = $db->prepare('INSERT INTO orderItems ("order_id", "menu_item_id", "quantity") VALUES (:order_id, :menu_item_id, :quantity)'); // einzelne Einträge in orderItems, order_Id bleibt dieselbe
     $stmt2->bindValue(':order_id', $order_id);
     $stmt2->bindValue(':menu_item_id', $menu_item_id);
     $stmt2->bindValue(':quantity', $quantity);
-
-
+    
     $result = $stmt2->execute();
-} 
+}
 
-// Idee dahinter:  - menuItems werden pro Bestellung durchiteriert und hinzugefügt
-//                 - es gibt derzeit 3 menuItems (Vanille, Schoko, Erdbeere), Schleife ist beendet, wenn die Ids der Eissorten durch sind
-//                 - quantity ist Nullable, wenn also eine Sorte nicht ausgewählt wurde, wird dafür auch nichts aufgerechnet
-//                 - if-Statement, if menu_item_id !0 ...
+// Prepared-Statements schützen vor SQL-Injection, indem sie verhindern, dass Eintragungen als SQL-Code gelesen werden können. Sie werden als reine Daten eingetragen.
 
-// ein prepared statement mehrmals ausführen in einer Schleife, um die einzelnen Menu_Items einzufügen
